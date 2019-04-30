85a5ef3b158dcea666ba5f18d8ccd844f7bf0848
Herisatry Lubaba <herisatry@gmail.com>
2019-05-01 01:16:58 +0300

    json file update

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,29 @@
 <template>
   <div id="app">
     <router-view/>
-    <footer
-      id="footer"
-      class="container-fluid pt-4 bg-light">
+    <footer id="footer" class="container-fluid pt-4 bg-light">
       <div class="container">
         <div class="row">
           <div class="col-6">
             <h5>About</h5>
             <ul class="list-unstyled text-small">
-              <li><a
-                class="text-muted"
-                href="https://github.com/iSm1le">Developer</a></li>
-              <li><a
-                class="text-muted"
-                href="https://github.com/iSm1le/checkme-xaff/blob/production/LICENSE">License</a></li>
+              <li>
+                <a class="text-muted" href="http://herisatry.com/">Who is the developer ?</a>
+              </li>
+              <li>
+                <a
+                  class="text-muted"
+                  href="https://github.com/herisatry/project-zero-remastered/blob/master/LICENSE"
+                >License</a>
+              </li>
             </ul>
           </div>
           <div class="col-6 text-right pt-4">
             <small class="d-block text-muted">herisatry.com &copy; 2019</small>
-            <small class="d-block text-muted pt-2"><b>Version: </b>{{ appVer }}</small>
+            <small class="d-block text-muted pt-2">
+              <b>Version:</b>
+              {{ appVer }}
+            </small>
           </div>
         </div>
       </div>
@@ -28,10 +32,10 @@
 </template>
 
 <script>
-import { version } from '../package.json';
+import { version } from "../package.json";
 
 export default {
-  name: 'App',
+  name: "App",
   computed: {
     appVer() {
       return version;
@@ -41,23 +45,24 @@ export default {
 </script>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css?family=Roboto:400,500,700');
+@import url("https://fonts.googleapis.com/css?family=Roboto:400,500,700");
 
 * {
   box-sizing: border-box;
-  font-family: 'Roboto', sans-serif;
+  font-family: "Roboto", sans-serif;
 }
 html,
 body,
 #app {
   height: 100%;
-  background: #222 url('./assets/img/pattern.png') repeat;
+  background: #007B5E url("./assets/img/pattern.png") repeat;
 }
 img {
   max-width: 100%;
 }
 
 footer {
+  background: black;
   position: fixed;
   left: 0;
   bottom: 0;
